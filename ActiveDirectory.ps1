6c80cb80ae4d16441688faaac5391f0a7e7c8269
Thiago Rocha do Nascimento <thiago.r.nascimento@hotmail.com>
2020-11-04 16:05:18 -0300

    Gerenciamento de Grupos Adicionado

--- ActiveDirectory.ps1
+++ ActiveDirectory.ps1
@@ -1,21 +1,24 @@
 # Active Directory
-
-install-windowsfeature AD-Domain-Services
+Add-WindowsFeature AD-Domain-Services
 
 # Criar Dominio
 Import-Module ADDSDeployment
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "thiago.local" -DomainNetbiosName "THIAGO" -ForestMode "WinThreshold" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
-
-# Adicionando um computador ao dominio
-Add-Computer -DomainName "thiago-local"
-Add-Computer -ComputerName "Server01" -LocalCredential Server01\Admin01 -Restart -Force # With Credentials
+Install-ADDSForest -DomainName thiago.local -InstallDNS
 
 # Criando uma OU
-New-ADOrganizationalUnit -Name "Senai" -Path "DC=THIAGO" -ProtectedFromAccidentalDeletion $False
+Import-Module ActiveDirectory
+New-ADOrganizationalUnit -Name "Senai" -Path "DC=thiago,DC=local" -ProtectedFromAccidentalDeletion $False
 
 # Cria um usuario AD
-New-ADUser -Name "Thiago" -AccountPassword "password_with_policy" -Enabled $true -PasswordNeverExpires $true -PasswordNotRequired $true -Path "DC=THIAGO/Users" -CannotChangePassword $true -ChangePasswordAtLogon $true -ScriptPath "DC=THIAGO/Scripts" -UserPrincipalName "thiago.rocha@thiago.local"
+$Senha = ConvertTo-SecureString "password_with_policy" -AsPlainText -Force
+New-ADUser -Name "Thiago" -SamAccountName "thiago.nascimento" -AccountPassword $Senha -Enabled $true -Path "OU=Senai,DC=thiago,DC=local" -ChangePasswordAtLogon $true 
 
 # Cria um Grupo de Usuarios AD 
-New-ADGroup -Name "Admins" -SamAccountName "Admins" -GroupCategory "Security" -GroupScope "Global" -DisplayName "Administrators" -Path "DC=THIAGO/Groups" -Description "Members of this group are Administrators"
+New-ADGroup -Name "Admins" -SamAccountName "Admins" -GroupCategory "Security" -GroupScope "Global" -DisplayName "Administrators" -Path "OU=Senai,DC=thiago,DC=local" -Description "Members of this group are Administrators"
 
+# Adiciona usuario à grupo
+Add-ADGroupMember -Identity Admins -Members thiago.nascimento
+
+# Deleta OU protegida contra exclusão acidental
+Get-ADOrganizationalUnit -Identity 'OU=Senai,DC=thiago,DC=local' | Set-ADObject -ProtectedFromAccidentalDeletion $False
+Remove-ADOrganizationalUnit -Identity 'OU=Senai,DC=thiago,DC=local'
